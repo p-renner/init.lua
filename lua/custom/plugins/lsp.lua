d031ce9e872583c87cdc91452582bb0541387647
Philipp Renner <phillianus@gmail.com>
2024-03-01 13:34:31 +0100

    feat(plugins): moved plugins to own folder

---
+++ lua/custom/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Automatically install LSPs to stdpath for neovim
+            { 'williamboman/mason.nvim', config = true },
+            'williamboman/mason-lspconfig.nvim',
+
+            -- Useful status updates for LSP
+            { 'j-hui/fidget.nvim',       opts = {} },
+
+            -- Additional lua configuration, makes nvim stuff amazing!
+            'folke/neodev.nvim',
+        },
+    },
+}
